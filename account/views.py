fbfc5c6074b8f07422718f2da1896c814d26b532
= <=>
2024-04-16 17:07:00 +0300

    We created an authentication system.

--- account/views.py
+++ account/views.py
@@ -1,3 +1,33 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+from .forms import LoginForm
+
+
+def user_login(request):
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            form_data = form.cleaned_data
+            user = authenticate(request, username=form_data['username'], password=form_data['password'])
+
+            if user is not None:
+                if user.is_active:
+                    login(request, user)
+                    return HttpResponse("Authenticated successfully")
+
+                return HttpResponse("Disabled account")
+
+            return HttpResponse("Invalid login")
+        
+
+    else:
+        form = LoginForm()
+
+    return render(request, 'account/login.html', {'form': form})
+
+@login_required
+def dashboard(request):
+    return render(request, "account/dashboard.html", {"section": "dashboard"})
