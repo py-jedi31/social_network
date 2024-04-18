a4fb086f535668f2d5948b2ca70a26c0a75fb43f
= <=>
2024-04-18 20:06:51 +0300

    Profile model was created. We added an opportunity to edit the user's profile. Also the message framework was plugged.

--- account/forms.py
+++ account/forms.py
@@ -1,5 +1,35 @@
 from django import forms
+from django.contrib.auth.models import User
+from .models import Profile
+
 
 class LoginForm(forms.Form):
     username = forms.CharField()
     password = forms.CharField(widget=forms.PasswordInput)
+
+class UserRegistrationForm(forms.ModelForm):
+    password = forms.CharField(label='Password', widget=forms.PasswordInput)
+    password2 = forms.CharField(label='Repeat password', widget=forms.PasswordInput)
+
+    class Meta:
+        model = User
+        fields = ['username', 'first_name', 'email']
+
+    def clean_password2(self):
+        form_data = self.cleaned_data
+        if form_data['password'] != form_data['password2']:
+            raise forms.ValidationError('Password don\'t match.')
+        return form_data['password2']
+
+class UserEditForm(forms.ModelForm):
+
+    class Meta:
+        model = User
+        fields = ['username', 'first_name', 'email']
+
+
+class ProfileEditForm(forms.ModelForm):
+
+    class Meta:
+        model = Profile
+        fields = ['date_of_birth', 'photo']
--- account/views.py
+++ account/views.py
@@ -1,9 +1,12 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.contrib import messages
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.decorators import login_required
 
-from .forms import LoginForm
+from .forms import LoginForm, UserRegistrationForm, UserEditForm, \
+        ProfileEditForm
+from .models import Profile
 
 
 def user_login(request):
@@ -28,6 +31,40 @@ def user_login(request):
 
     return render(request, 'account/login.html', {'form': form})
 
+
 @login_required
 def dashboard(request):
     return render(request, "account/dashboard.html", {"section": "dashboard"})
+
+
+def register(request):
+    if request.method == "POST":
+        user_form = UserRegistrationForm(request.POST)
+        if user_form.is_valid():
+            new_user = user_form.save(commit=False)
+
+            new_user.set_password(user_form.cleaned_data['password'])
+            new_user.save()
+            Profile.objects.create(user=new_user)
+            return render(request, 'account/register_done.html', {'new_user': new_user})
+
+    user_form = UserRegistrationForm()
+    return render(request, 'account/register.html', {'user_form': user_form})
+
+def edit(request):
+    if request.method == "POST":
+        user_form = UserEditForm(instance=request.user, data=request.POST)
+        profile_form = ProfileEditForm(instance=request.user.profile, 
+                data=request.POST, files=request.FILES)
+        if user_form.is_valid() and profile_form.is_valid():
+            user_form.save()
+            profile_form.save()
+            messages.success(request, "Profile has been edited successfully")
+        else:
+            messages.error(request, "Profile hasn't been changed.")
+    else:
+        user_form = UserEditForm(instance=request.user)
+        profile_form = ProfileEditForm(instance=request.user.profile)
+
+
+    return render(request, "account/edit.html", {"user_form": user_form, "profile_form": profile_form})
